174c5561d348b074b118f4567c83d9824d4bcea4
Uedson Reis <uedsonreis@MacReis.local>
2021-08-16 17:24:55 -0300

    WIP

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -10,13 +10,12 @@ export class UserService {
 
     constructor() {
         this.firebaseAdmin = admin.initializeApp({
-            // credential: admin.credential.cert({
-                // projectId: process.env.PROJECT_ID,
-                // privateKey: process.env.PRIVATE_KEY,
-                // clientEmail: process.env.SERVICE_ACCOUNT_ID,
-            // }),
+            credential: admin.credential.cert({
+                projectId: process.env.PROJECT_ID,
+                privateKey: process.env.PRIVATE_KEY,
+                clientEmail: process.env.SERVICE_ACCOUNT_ID,
+            }),
             projectId: process.env.PROJECT_ID,
-            storageBucket: process.env.STORAGE_BUCKET,
             serviceAccountId: process.env.SERVICE_ACCOUNT_ID,
         })
     }
