9f7fc993509c5396fddc91c5da918d731947ae20
Uedson Reis <uedsonreis@MacReis.local>
2021-08-17 17:13:55 -0300

    WIP

--- src/posts/post.service.ts
+++ src/posts/post.service.ts
@@ -1,15 +1,32 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
-import { Transaction } from 'sequelize/types'
+import { IncludeOptions, Transaction } from 'sequelize/types'
 
 import { AbstractService } from '../api/abstract.service'
 import Post from './post.entity'
+
+const include = JSON.stringify([{ association: "owner" }])
 
 @Injectable()
 export class PostService extends AbstractService<Post> {
 
     constructor(@InjectModel(Post) repository: typeof Post) {
         super(repository)
+    }
+
+    public async getById(id: number, options: any): Promise<Post | null> {
+        const postDB = await super.getById(id, { ...options, include })
+        return this.removeOwnerId(postDB)
+    }
+
+    public async getList(filters: any): Promise<Post[]> {
+        const list = await super.getList({ ...filters, include })
+        return list.map(this.removeOwnerId)
+    }
+
+    private removeOwnerId(post: Post) {
+        const { ownerId, ...rest } = post.toJSON() as Post
+        return rest as Post
     }
 
     public async update(id: number, record: Post, transaction?: Transaction) {
